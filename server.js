834173856bd758da7656adc0e2c63bccf127dd93
Donovan <donovan6925@gmail.com>
2017-03-31 15:52:47 -0400

    added env variable to server

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const server = new Hapi.Server({
         }
     }
 });
-server.connection({ port: 3000 });
+server.connection({ port: ~~process.env.PORT || 3000 });
 
 server.register(Inert, (err) => {
     if (err) {
