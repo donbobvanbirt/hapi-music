63a6874ab4a27d0b905e524c3a262823b0b00c30
Donovan <donovan6925@gmail.com>
2017-03-31 15:26:33 -0400

    cleaning up

--- server.js
+++ server.js
@@ -1,33 +1,5 @@
 'use strict';
 
-// const Hapi = require('hapi');
-//
-// // Create a server with a host and port
-// const server = new Hapi.Server();
-// server.connection({
-//     host: 'localhost',
-//     port: 8000
-// });
-//
-// // Add the route
-// server.route({
-//     method: 'GET',
-//     path:'/',
-//     handler: function (request, reply) {
-//
-//         return reply('hello world');
-//     }
-// });
-//
-// // Start the server
-// server.start((err) => {
-//
-//     if (err) {
-//         throw err;
-//     }
-//     console.log('Server running at:', server.info.uri);
-// });
-
 const Path = require('path');
 const Hapi = require('hapi');
 const Inert = require('inert');
@@ -43,19 +15,11 @@ const server = new Hapi.Server({
 });
 server.connection({ port: 3000 });
 
-server.register(Inert, () => {});
-
-// server.route({
-//     method: 'GET',
-//     path: '/{param*}',
-//     handler: {
-//         directory: {
-//             path: '.',
-//             redirectToSlash: true,
-//             index: true
-//         }
-//     }
-// });
+server.register(Inert, (err) => {
+    if (err) {
+        throw err;
+    }
+});
 
 server.route({
     method: 'GET',
@@ -81,19 +45,7 @@ server.route({
     }
 });
 
-// server.route({
-//     method: 'GET',
-//     path: '/{user?}',
-//     handler: function (request, reply) {
-//         reply({ file: `${encodeURIComponent(request.params.user)}.html` });
-//     }
-//     // handler: {
-//     //     file: `${request.params.user}.html`
-//     // }
-// });
-
 server.start((err) => {
-
     if (err) {
         throw err;
     }
